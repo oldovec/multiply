226326f8ba29c3dea684de40478a665926aa2496
Maksim <Oldovec.cool@gmail.com>
2019-10-13 20:04:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function multiply(first, second) {
-  // your solution
+let massFirst=first.split("").reverse();
+  let massSecond=second.split("").reverse();
+  let massSum=[];
+  for(i=0;i<massFirst.length;i++) {
+      for(j=0;j<massSecond.length;j++)
+      {
+          massSum[i+j]? massSum[i+j]+=massFirst[i]*massSecond[j] : massSum[i+j]=massFirst[i]*massSecond[j];
+      }
+  }
+  for(i=0;i<massSum.length-1;i++) {
+      massSum[i+1]+=massSum[i]/10|0;
+      massSum[i]%=10;
+      if(i==massSum.length-2 && massSum[massSum.length-1]>9) 
+          {
+              massSum[massSum.length]=0;
+          }
+  }
+  let resultMultiply=massSum.reverse().join('');
+  return resultMultiply;
 }
